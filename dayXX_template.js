776d37462ff0cac78ae0cfc03a0da8682e5f1494
Ruben Valseca <rubn.vp@gmail.com>
2022-10-07 22:32:20 +0200

    create dayXX template

---
+++ dayXX_template.js
@@ -0,0 +1,30 @@
+import { readFileSync } from 'fs';
+console.time('✨ Done in');
+console.log('--- DayXX ---');
+
+// const inputText = readFileSync('./inputs/dayXX.txt', {encoding:'utf8'});
+const inputText = `
+
+`; // test input
+
+function parseInput(inputText) {
+  return inputText.trim();
+}
+
+function solve1(input) {
+  return input;
+}
+
+// function solve2(input) {
+//   return input;
+// }
+
+const input = parseInput(inputText);
+
+const solution1 = solve1(input);
+console.log('solution 1:', solution1);
+
+// const solution2 = solve2(input);
+// console.log('solution 2:', solution2);
+
+console.timeEnd('✨ Done in');
